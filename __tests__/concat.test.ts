ca2a2f81fea515c25392889aeac8026dbb4c0a5c
simone <simonegauli@gmail.com>
2023-08-29 10:43:00 +0200

    improve test

--- __tests__/concat.test.ts
+++ __tests__/concat.test.ts
@@ -1,6 +1,6 @@
 import { describe, it, jest } from '@jest/globals';
 import { fileURLToPath } from 'node:url';
-import concat from '../src/index';
+import concat from '../src';
 import path from 'node:path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
