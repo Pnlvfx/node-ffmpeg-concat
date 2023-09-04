935a96bf2094b2be5c89932cc1d1c22e469157d1
simone <simonegauli@gmail.com>
2023-09-04 21:11:53 +0200

    fix

--- src/index.ts
+++ src/index.ts
@@ -109,16 +109,16 @@ export default concat;
 
 export { default as transitions } from './helpers/transitions-wrap.js';
 
-// RUN EXAMPLE
+// // RUN EXAMPLE
 
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const getFile = (file: string) => path.join(__dirname, '..', '..', file);
+// import path from 'node:path';
+// import { fileURLToPath } from 'node:url';
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+// const getFile = (file: string) => path.join(__dirname, '..', '..', file);
 
-concat({
-  videos: [getFile('media/0.mp4'), getFile('media/0a.mp4'), getFile('media/1.mp4'), getFile('media/2.mp4')],
-  output: './media/example.mp4',
-  transition: { name: 'directionalwipe', duration: 500 },
-});
+// concat({
+//   videos: [getFile('media/0.mp4'), getFile('media/0a.mp4'), getFile('media/1.mp4'), getFile('media/2.mp4')],
+//   output: './media/example.mp4',
+//   transition: { name: 'directionalwipe', duration: 500 },
+// });
