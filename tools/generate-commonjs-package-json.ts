4068aaf53dc6eb41e47bcf2bf8ab89241644398c
simone <simonegauli@gmail.com>
2023-09-06 13:17:30 +0200

    fix error with commonjs import

---
+++ tools/generate-commonjs-package-json.ts
@@ -0,0 +1,10 @@
+import { mkdirSync, writeFileSync } from 'node:fs';
+import { dirname } from 'node:path';
+
+/**
+ * Outputs the dummy package.json file to the path specified
+ * by the first argument.
+ */
+
+mkdirSync(dirname(process.argv[2]), { recursive: true });
+writeFileSync(process.argv[2], `{"type": "commonjs"}`);
