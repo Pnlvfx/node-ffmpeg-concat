951fbc0a964b03730d3586e8d4bd14d3fe7e5722
simone <simonegauli@gmail.com>
2025-07-31 21:28:38 +0200

    start config migration

--- tools/generate-commonjs-package-json.ts
+++
@@ -1,9 +0,0 @@
-import fs from 'node:fs/promises';
-import path from 'node:path';
-
-const output = process.argv[2];
-
-if (!output) throw new Error('No output specified.');
-
-await fs.mkdir(path.dirname(output), { recursive: true });
-await fs.writeFile(output, `{"type": "commonjs"}`);
