44bc320fbf756f9a62d9443aa5507abb78e0c73b
Simone Gauli <simone97_19@hotmail.it>
2025-08-01 16:18:13 +0200

    bump jest

--- __tests__/concat.test.ts
+++ __tests__/concat.test.ts
@@ -10,39 +10,15 @@ describe('concat function', () => {
     'should concatenate videos successfully',
     async () => {
       const mockEnsureDirSync = jest.fn();
-      // const mockInitFrames = jest.fn(() => Promise.resolve({ frames: [], scenes: [], theme: {} }));
-      // const mockRenderFrames = jest.fn(() => Promise.resolve('framePattern'));
-      // const mockRenderAudio = jest.fn(() => Promise.resolve('audioFile'));
-      // const mockTranscodeVideo = jest.fn(() => Promise.resolve());
-
       jest.mock('fs-extra', () => ({
         ensureDirSync: mockEnsureDirSync,
       }));
-
-      // jest.mock('../src/helpers/init-frames', () => ({
-      //   initFrames: mockInitFrames,
-      // }));
-      // jest.mock('../src/helpers/render-frames', () => ({
-      //   renderFrames: mockRenderFrames,
-      // }));
-      // jest.mock('../src/helpers/render-audio', () => ({
-      //   renderAudio: mockRenderAudio,
-      // }));
-      // jest.mock('../src/helpers/transcode-video', () => ({
-      //   transcodeVideo: mockTranscodeVideo,
-      // }));
 
       await concat({
         videos: [getFile('media/0.mp4'), getFile('media/0a.mp4'), getFile('media/1.mp4'), getFile('media/2.mp4')],
         output: path.join('media', `example_${os.platform()}.mp4`),
         transition: { name: 'directionalwipe', duration: 500, params: {} },
       });
-
-      // Perform assertions to ensure mock functions were called as expected
-      // expect(mockInitFrames).toHaveBeenCalled();
-      // expect(mockRenderFrames).toHaveBeenCalled();
-      // expect(mockRenderAudio).toHaveBeenCalled();
-      // expect(mockTranscodeVideo).toHaveBeenCalled();
     },
     2 * 60 * 1000,
   );
